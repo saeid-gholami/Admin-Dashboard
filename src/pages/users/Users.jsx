e3993b2bba5afa28ed7e886bdc2f83cbb25a5cbd
Saeid Gholami <gholami_0101@yahoo.com>
2025-01-14 16:19:59 +0330

    refactor

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -10,7 +10,6 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
 
 
 export default function Users() {
